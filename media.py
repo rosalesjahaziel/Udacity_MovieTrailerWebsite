34b172dedd1973b69c80a4cf77389f7814a3b156
rosalesjahaziel <rosales.jahaziel.a@gmail.com>
2017-03-21 19:18:28 -0500

    add pep8 standards

--- media.py
+++ media.py
@@ -1,18 +1,25 @@
 import webbrowser
 
+
 class TrailerInfo():
-    
+    "This is the parent clase how content the basic infomation for a video"
+
     def __init__(self, trailer_title, trailer_image, trailer_youtube):
+        "an isinstance have title, img poster and url youtube video"
         self.title = trailer_title
         self.poster_image_url = trailer_image
         self.trailer_youtube_url = trailer_youtube
 
-class Movie(TrailerInfo):
 
-    def __init__(self,trailer_title, trailer_image, trailer_youtube):
-        TrailerInfo.__init__(self, trailer_title, trailer_image, trailer_youtube)
+class Movie(TrailerInfo):
+    "Init class using parent TrailerInfo Class"
+    def __init__(self, trailer_title, trailer_image, trailer_youtube):
+        TrailerInfo.__init__(self, trailer_title,
+                            trailer_image, trailer_youtube)
+
 
 class Games(TrailerInfo):
-   
-    def __init__(self,trailer_title, trailer_image, trailer_youtube):
-        TrailerInfo.__init__(self, trailer_title, trailer_image, trailer_youtube)
+    "Init class using parent TrailerInfo Class"
+    def __init__(self, trailer_title, trailer_image, trailer_youtube):
+        TrailerInfo.__init__(self, trailer_title,
+                            trailer_image, trailer_youtube)
